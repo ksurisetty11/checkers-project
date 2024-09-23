340cb26eef4ee0421d21391f1bb0f81f328848e7
meheroke <meheroke@gmail.com>
2024-09-23 16:46:20 -0400

    added descrption to CheckersFE

--- cpsc2150/extendedCheckers/views/CheckersFE.java
+++ cpsc2150/extendedCheckers/views/CheckersFE.java
@@ -1,5 +1,11 @@
 package cpsc2150.extendedCheckers.views;
-
+/**
+ * CheckersFE currently contains the main method for the checkers game.
+ * In future projects, the main will handle user input and be responsible for running
+ * the entire checkers game
+ * The main will control the gameplay and is where all of the functions for checkers
+ * will be called.
+ */
 public class CheckersFE
 {
     public static void main(String[] args)
