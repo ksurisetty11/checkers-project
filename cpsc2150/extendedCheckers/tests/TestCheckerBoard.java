3f5296beb510db60c946861a155bb2141971fe44
Laura Castro Rosales <lcastr2@clemson.edu>
2024-10-17 16:39:19 -0400

    added blue functions w/o code

--- cpsc2150/extendedCheckers/tests/TestCheckerBoard.java
+++ cpsc2150/extendedCheckers/tests/TestCheckerBoard.java
@@ -1,1 +1,54 @@
+package cpsc2150.extendedCheckers.tests;
+import org.junit.*;
+import static org.junit.Assert.*;
 
+
+public class TestCheckerBoard {
+    @Test
+    public void test_CheckerBoard_Constructor()
+    {
+
+    }
+
+    @Test
+    public void test_whatsAtPos_MinRowMinCol()
+    {
+
+    }
+
+    @Test
+    public void test_whatsAtPos_MaxRowMaxCol()
+    {
+
+    }
+
+    @Test
+    public void test_whatsAtPos_MidRowMidCol()
+    {
+
+    }
+
+    @Test
+    public void test_whatsAtPos_BlackTile()
+    {
+
+    }
+
+    @Test
+    public void test_whatsAtPos_WhiteTile()
+    {
+
+    }
+
+    @Test
+    public void test_whatsAtPos_EmptyTile()
+    {
+
+    }
+
+    @Test
+    public void test_getPieceCounts_x12_o12()
+    {
+
+    }
+}
