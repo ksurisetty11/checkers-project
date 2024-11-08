79c57226d1d2caf6adece92c5a016f80bf9b6d82
Laura Castro Rosales <lcastr2@clemson.edu>
2024-11-07 19:46:20 -0500

    changed comment on driver

--- cpsc2150/extendedCheckers/views/CheckersFE.java
+++ cpsc2150/extendedCheckers/views/CheckersFE.java
@@ -10,12 +10,8 @@ public class CheckersFE
 {
     public static void main(String[] args)
     {
-        /*
-        Main function. Remember, for project 1 we aren't implementing anything yet.
 
-        You are welcome to add your own helper functions to this file. However, those functions must be given contracts.
 
-        Also, the main typically doesn't need a contract. But you should still write the class contract.
-         */
+        //You are welcome to add your own helper functions to this file. However, those functions must be given contracts.
     }
 }
