25dcd2ad5fde615e3dca7e3008b8709dd6b2e920
Nadia <nalexa3@clemson.edu>
2024-11-22 22:19:02 -0500

    Updated driver for updated functionality

--- cpsc2150/extendedCheckers/views/CheckersFE.java
+++ cpsc2150/extendedCheckers/views/CheckersFE.java
@@ -14,107 +14,34 @@ import static cpsc2150.extendedCheckers.models.ICheckerBoard.getDirection;
  * CheckersFE currently contains the main method for the checkers game.
  * In future projects, the main will handle user input and be responsible for running
  * the entire checkers game
- * The main will control the gameplay and is where all of the functions for checkers
+ * The main will control the gameplay and is where all the functions for checkers
  * will be called.
  */
 public class CheckersFE {
     public static final Scanner readInInput = new Scanner(System.in);
 
     public static void main(String[] args) {
-        ICheckerBoard gameBoard = new CheckerBoard();
-        HashMap<Character, ArrayList<DirectionEnum>> gameDir = gameBoard.getViableDirections();
+        ICheckerBoard gameBoard = new CheckerBoard(8);
         Scanner readInInput = new Scanner(System.in);
         boolean gameOn = true;
-
         //Game starts with player x
         char whichPlayer = CheckerBoard.PLAYER_ONE;
 
         //Loop that switches control of each player each iteration
-        while (gameOn)
-        {
+        while (gameOn) {
+            //Current State of the board
             String currentBoard = gameBoard.toString();
             System.out.println(currentBoard);
 
+            //Validation for the correct piece chosen
             BoardPosition startPos = getPieceCoordinates(whichPlayer, gameBoard);
+            DirectionEnum intendedDir = getValidPieceDirections(gameBoard, startPos);
 
-            //Input validation for getting the direction
-            boolean isValidDirection = true;
-            DirectionEnum intendedDir = null;
-            do
-            {
-                HashMap<DirectionEnum, Character> availableMoves = gameBoard.scanSurroundingPositions(startPos);
-                System.out.println("In which direction do you wish to move the piece? " +
-                        "Enter one of these options:");
 
-                //Checking for null values
-                char checkPosition = gameBoard.whatsAtPos(startPos);
-                ArrayList<DirectionEnum> possibleMoves = gameBoard.getViableDirections().get(checkPosition);
-                if (possibleMoves != null) {
-                    for (DirectionEnum possibleDirections : possibleMoves) {
-                        if (availableMoves.containsKey(possibleDirections)) {
-                            System.out.println(possibleDirections);
-                        }
-                    }
-                }
-                else {
-                    System.out.println("No possible moves.");
-                }
+            //Validation for getting the allowed directions
+            movePieceOnBoard(startPos, gameBoard, intendedDir);
 
-                String directionToMove = readInInput.next().toUpperCase();
-
-                //Checking for values other than what's in DirectionEum
-                if (directionToMove.contains("NE" ))
-                {
-                    intendedDir = DirectionEnum.NE;
-                }
-                else if (directionToMove.contains("NW"))
-                {
-                    intendedDir = DirectionEnum.NW;
-                }
-                else if (directionToMove.contains("SE"))
-                {
-                    intendedDir = DirectionEnum.SE;
-                }
-                else if (directionToMove.contains("SW"))
-                {
-                    intendedDir = DirectionEnum.SW;
-                }
-                else
-                {
-                    System.out.println("Invalid direction.");
-                }
-
-                //Checking that the chosen direction is valid
-                intendedDir = DirectionEnum.valueOf(directionToMove);
-                if (!(availableMoves.containsKey(intendedDir))) {
-                    System.out.println("Unable to move in that direction.");
-                }
-                if (availableMoves.containsKey(directionToMove)) {
-                    isValidDirection = true;
-                }
-
-            } while (!isValidDirection);
-
-            //Moving the piece in the specified direction
-            BoardPosition firstPos = ICheckerBoard.getDirection(intendedDir);
-            BoardPosition moveToPos = new BoardPosition(startPos.getRow() +
-                    firstPos.getRow(),startPos.getColumn() + firstPos.getColumn());
-
-            if (gameBoard.whatsAtPos(moveToPos) == CheckerBoard.EMPTY_POS) {
-                BoardPosition endPos = gameBoard.movePiece(new BoardPosition(startPos.getRow(),
-                                       startPos.getColumn()), intendedDir);
-                gameBoard.crownPiece(endPos);
-            }
-            else
-            {
-                BoardPosition endPos = gameBoard.jumpPiece(new BoardPosition(startPos.getRow(),
-                                       startPos.getColumn()), intendedDir);
-                gameBoard.crownPiece(endPos);
-            }
-
-            //Ending or repeating the games if a player has won
-            if (gameBoard.checkPlayerWin(whichPlayer))
-            {
+            if (gameBoard.checkPlayerWin(whichPlayer)) {
                 System.out.println("Player " + whichPlayer + " has won!");
                 boolean keepPlaying = true;
                 boolean endGame = true;
@@ -130,7 +57,7 @@ public class CheckersFE {
 
                 if (keepPlaying)
                 {
-                    gameBoard = new CheckerBoard();
+                    gameBoard = new CheckerBoard(8);
                     whichPlayer = CheckerBoard.PLAYER_ONE;
                 }
                 else
@@ -139,54 +66,151 @@ public class CheckersFE {
                 }
 
             }
-
             //If the game hasn't ended yet, it is the other players turn to move a piece
-            if (whichPlayer == CheckerBoard.PLAYER_ONE) { whichPlayer = CheckerBoard.PLAYER_TWO; }
-            else { whichPlayer = CheckerBoard.PLAYER_ONE; }
+            if (whichPlayer == CheckerBoard.PLAYER_ONE) {
+                whichPlayer = CheckerBoard.PLAYER_TWO;
+            }
+            else {
+                whichPlayer = CheckerBoard.PLAYER_ONE;
+            }
         }
     }
 
     /**
      * Prompts the player to select a piece to move
      * The method repeatedly asks the player to choose valid coordinates to move their pieces.
-     * @param whichPlayer The character that represents the player 'x' or 'o'
-     * @param gameBoard The checkerboard that implements the interface
      *
+     * @param whichPlayer The character that represents the player 'x' or 'o'
+     * @param gameBoard   The checkerboard that implements the interface
      * @return a BoardPosition object representing the coordinates of the selected piece
      * @pre whichPlayer needs to be 'x', 'X', 'o', 'O'
-     * @post getPieceCoordinates = [BoardPosition that was chosen by users]
+     * @post getPieceCoordinates = BoardPosition [BoardPosition that was chosen by users]
      */
     private static BoardPosition getPieceCoordinates(char whichPlayer, ICheckerBoard gameBoard) {
         int inputRow, inputCol;
         boolean isValidPiece = false;
-
         do {
             System.out.println("Player " + whichPlayer + ", which piece do you wish to move?" +
                     " Enter the row followed by a space followed by the column.");
             inputRow = readInInput.nextInt();
             inputCol = readInInput.nextInt();
 
-            //Checking if the user inputted coordinate is within the board boundary
-            boolean withinBounds = (inputRow >= 0 && inputRow < CheckerBoard.ROW_NUM)
-                    && (inputCol >= 0 && inputCol < CheckerBoard.COL_NUM);
-
-            if (!withinBounds)
-            {
+            boolean withinBounds = (inputRow >= 0 && inputRow < gameBoard.getRowNum()) &&
+                    (inputCol >= 0 && inputCol < gameBoard.getColNum());
+            if (!withinBounds) {
                 System.out.println("Out of bounds! Choose a different coordinate.");
                 continue;
             }
 
-            //Checking that the player chose their own piece
             char pieceAtPos = gameBoard.whatsAtPos(new BoardPosition(inputRow, inputCol));
-            if ((pieceAtPos == whichPlayer) || (pieceAtPos == Character.toUpperCase(whichPlayer)))
-            {
+            if ((pieceAtPos == whichPlayer) || (pieceAtPos == Character.toUpperCase(whichPlayer))) {
                 isValidPiece = true;
-            } else
-            {
+            } else {
                 System.out.println("Player " + whichPlayer + ", that isn't your piece. Pick one of your pieces.");
             }
         } while (!isValidPiece);
-
         return new BoardPosition(inputRow, inputCol);
+    }
+
+    private static DirectionEnum getValidPieceDirections(ICheckerBoard gameBoard, BoardPosition startPos) {
+        //Input validation for getting the direction
+        boolean isValidDirection = true;
+        DirectionEnum intendedDir = null;
+        char checkPosition = gameBoard.whatsAtPos(startPos);
+        ArrayList<DirectionEnum> officialAvailableMoves = new ArrayList<>();
+
+        HashMap<DirectionEnum, Character> availableMoves = gameBoard.scanSurroundingPositions(startPos);
+        ArrayList<DirectionEnum> possibleMoves = gameBoard.getViableDirections().get(checkPosition);
+
+        if (possibleMoves != null) {
+            for (DirectionEnum direction : possibleMoves) {
+                if ((availableMoves.containsKey(direction)) && (availableMoves.get(direction) == ' ')) {
+                    officialAvailableMoves.add(direction);
+                }
+                else if (availableMoves.containsKey(direction) && availableMoves.get(direction) != ' ') {
+                    BoardPosition testDirection = getDirection(direction);
+
+                    BoardPosition endedJumpPosition = new BoardPosition(startPos.getRow() +
+                            (2 * testDirection.getRow()), startPos.getColumn() + (2 * testDirection.getColumn()));
+                    boolean withinRowBoundaries = ((endedJumpPosition.getRow() >= 0) &&
+                            (endedJumpPosition.getRow() < gameBoard.getRowNum()));
+                    boolean withinColBoundaries = ((endedJumpPosition.getColumn() < gameBoard.getColNum())
+                            && (endedJumpPosition.getColumn() >= 0));
+
+                    if (withinRowBoundaries && withinColBoundaries && (gameBoard.whatsAtPos(endedJumpPosition) == ' ')) {
+                        officialAvailableMoves.add(direction);
+                    }
+                }
+            }
+        }
+
+        if (!officialAvailableMoves.isEmpty()) {
+            do {
+                System.out.println("In which direction do you wish to move the piece? " +
+                        "Enter one of these options:");
+                for (DirectionEnum dir : officialAvailableMoves) {
+                    System.out.println(dir);
+                }
+                String directionToMove = readInInput.next().toUpperCase();
+                boolean checkingValidPlayerDir = false;
+
+                for (DirectionEnum dir : DirectionEnum.values()) {
+                    if (dir.name().equalsIgnoreCase(directionToMove)) {
+                        checkingValidPlayerDir = true;
+                        intendedDir = dir;
+                        break;
+                    }
+                }
+
+                //intendedDir = DirectionEnum.valueOf(directionToMove);
+                if (checkingValidPlayerDir && officialAvailableMoves.contains(intendedDir)) {
+                    isValidDirection = true;
+                } else {
+                    System.out.println("Invalid direction.");
+                    isValidDirection = false;
+                }
+            } while (!isValidDirection);
+        }
+        return intendedDir;
+    }
+
+
+    private static void movePieceOnBoard(BoardPosition startPos, ICheckerBoard gameBoard, DirectionEnum dir)
+    {
+        //Direction the piece will be going
+        BoardPosition intendedDirCoord = ICheckerBoard.getDirection(dir);
+
+        //Validation for moving to that direction and not going out of bounds.
+        BoardPosition moveToPos = new BoardPosition(startPos.getRow() +
+                intendedDirCoord.getRow(),startPos.getColumn() + intendedDirCoord.getColumn());
+
+        //Validation to move the piece
+        if (gameBoard.whatsAtPos(moveToPos) == CheckerBoard.EMPTY_POS) {
+            BoardPosition endPos = gameBoard.movePiece(new BoardPosition(startPos.getRow(),
+                    startPos.getColumn()), dir);
+            canBeCrowned(endPos, gameBoard);
+        }
+        else
+        {
+            BoardPosition endPos = gameBoard.jumpPiece(new BoardPosition(startPos.getRow(),
+                    startPos.getColumn()), dir);
+            canBeCrowned(endPos, gameBoard);
+        }
+    }
+
+
+    private static void canBeCrowned(BoardPosition pos, ICheckerBoard gameBoard)
+    {   //if piece == player 1
+        if (gameBoard.whatsAtPos(pos) == CheckerBoard.PLAYER_ONE) {
+            if (pos.getRow() == gameBoard.getRowNum() - 1) {
+                gameBoard.crownPiece(pos);
+            }
+        }
+        //if piece == player 2
+        if (gameBoard.whatsAtPos(pos) == CheckerBoard.PLAYER_TWO) {
+            if (pos.getRow() == 0) {
+                gameBoard.crownPiece(pos);
+            }
+        }
     }
 }
